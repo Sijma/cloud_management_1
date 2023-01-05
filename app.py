6ac175da7cac2c95ffc41815847d8fecb3600f09
Sijma <johnbalasis@yahoo.gr>
2023-01-05 14:06:34 +0200

    added /Read operation and fetch_articles function in database

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
-from flask import Flask
-
-import kafkaProducer
+from flask import Flask, request
+import database
 
 app = Flask(__name__)
 
@@ -8,6 +7,22 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():  # put application's code here
     return 'Hello World!'
+
+
+@app.route("/Create", methods=["POST"])
+def create():
+    # Get the JSON payload from the request
+    data = request.get_json()
+
+    return database.register_user(data)
+
+
+@app.route("/Read", methods=["GET"])
+def read():
+    # Get the email address from the request query string
+    email = request.args.get("email")
+
+    return database.fetch_articles(email)
 
 
 if __name__ == '__main__':
--- database.py
+++ database.py
@@ -1,4 +1,5 @@
 import config
+import time
 import pymongo
 import json
 
@@ -7,9 +8,9 @@ client = pymongo.MongoClient("mongodb://localhost:27017/")
 db = client["cm_db"]
 
 # Check if the collection exists, and create it if it doesn't
-for keyword in config.keywords:
-    if keyword not in db.list_collection_names():
-        db.create_collection(keyword)
+for col in config.collections:
+    if col not in db.list_collection_names():
+        db.create_collection(col)
 
 
 def add_to_database(topic, message):
@@ -20,4 +21,56 @@ def add_to_database(topic, message):
     message_dict = json.loads(message)
 
     # Insert the message into the collection
-    collection.update_one(message_dict, {"$setOnInsert": {"key": "value"}}, upsert=True)
+    collection.update_one(message_dict, {"$setOnInsert": {}}, upsert=True)
+
+
+def register_user(data):
+    # Check if the required keys are present in the JSON payload
+    if "name" in data and "email" in data and "keywords" in data and "city" in data:
+        for keyword in data["keywords"]:
+            if keyword not in config.keywords:
+                return f"Error: {keyword} is not a valid keyword"
+
+        users_collection = db["users"]
+        user = users_collection.find_one({"email": data["email"]})
+
+        # If the user does not exist, insert the new user document
+        if not user:
+            timestamp = int(time.time())
+            data["timestamp"] = timestamp
+            users_collection.insert_one(data)
+            return "Operation Successful"
+        else:
+            return "Error: user with that email address already exists"
+    else:
+        return "Error: missing required keys"
+
+
+def fetch_articles(email):
+    # Find the user document in the collection with the matching email address
+    users_collection = db["users"]
+    user = users_collection.find_one({"email": email})
+
+    # If the user exists, get the keywords array and use it to find all the documents in the corresponding collections
+    if user:
+        articles_by_source = {}
+        for keyword in user["keywords"]:
+            collection = db[keyword]
+            docs = collection.find({}, {"_id": 0})
+            for doc in docs:
+                source_name = doc["source"]["name"]
+                if source_name not in articles_by_source:
+                    articles_by_source[source_name] = {"articles": []}
+                articles_by_source[source_name]["articles"].append(doc)
+
+        # For each source name, look up the description in the "sources_domain_name" collection and add it to the group
+        sources_collection = db["sources_domain_name"]
+        for source_name in articles_by_source:
+            source_doc = sources_collection.find_one({"domain_name": source_name})
+            if source_doc:
+                description = source_doc["description"]
+                articles_by_source[source_name]["Source Description"] = description
+
+        return{"articles": articles_by_source}
+    else:
+        return "Error: user not found"
